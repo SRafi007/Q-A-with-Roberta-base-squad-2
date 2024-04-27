e1e1c7d652198f366d9b3e7d834a6bc654995e2a
Sadman Sakib Rafi <88015364+SRafi007@users.noreply.github.com>
2024-04-27 22:12:22 +0600

    Update roberta.py

--- backend/roberta.py
+++ backend/roberta.py
@@ -4,8 +4,8 @@ from fastapi import FastAPI
 app = FastAPI()
 model_name = "deepset/roberta-base-squad2"
 
-question = 'Why is model conversion important?'
-context = 'The option to convert models between FARM and transformers gives freedom to the user and let people easily switch between frameworks.'
+question = 'Why is bangladesh important important?'
+context = 'Bangladesh officially the People's Republic of Bangladesh,[b] is a country in South Asia. It is the eighth-most populous country in the world and is among the most densely populated countries with a population of nearly 170 million in an area of 148,460 square kilometres (57,320 sq mi). Bangladesh shares land borders with India to the north, west, and east, and Myanmar to the southeast. To the south, it has a coastline along the Bay of Bengal. It is narrowly separated from Bhutan and Nepal by the Siliguri Corridor, and from China by the mountainous Indian state of Sikkim in the north. Dhaka, the capital and largest city, is the nation's political, financial, and cultural centre. Chittagong, the second-largest city, is the busiest port on the Bay of Bengal. The official language of Bangladesh is Bengali.'
 
 # Load model & tokenizer
 #model = AutoModelForQuestionAnswering.from_pretrained(model_name)
